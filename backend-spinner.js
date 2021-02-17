d8413db852b9f7078a4a286d04b43a05c7fa0ab0
Simon <simonlaursen@protonmail.com>
2021-02-17 14:58:38 +0100

    The spinner returns the index of the user that it is pointing at. It determines which user it is pointing at by comparing the angle between the point that the spinner points to, the position of the spinner and the user.
    If the game is played by only one user, this user will be selected.

--- backend-spinner.js
+++ backend-spinner.js
@@ -1,5 +1,5 @@
 
-//The current positions of users in px
+//The current positions of users in px (Use these for debugging and test.
 const userpos = [
     {top: 266, left: 216},
     {top: 218, left: 581},
@@ -7,34 +7,51 @@ const userpos = [
     {top: 469, left: 249}
 ];
 
+//The position of the spinner.
 const s_pos = {top: 336, left: 312};
 
-const getRelUserPos = ()=> {
-    const res = [];
-    userpos.forEach(value => res.push({
-        top:  value.top  - s_pos.top,
-        left: value.left - s_pos.left
-    }));
-    return res;
-}
+export function spin(userPos){
 
+    // Pass this to the front end to run the spinner locally.
+    const rot = Math.random() * 360;
 
-function closestUser(userPositions, fwd){
-    const relPos = getRelUserPos();
+    //return the result of the spin.
+    return closestUser(userPos, rot);
 
-    const op = x => fwd.scale(x);
+    //--- Helper functions ---\\
+    //Find the user which is closest to being pointed at. Pass this function so the front end can play along.
+    function closestUser(userPositions, rotDeg){
+        const relPos = getRelUserPos();
 
-    const up = x => fwd.ortho().scale(x) + b;
+        const rots = [];
 
-    const dist = [];
+        //Check the angles between all the users in the game.
+        relPos.forEach(value => {
+            let a = Math.atan(value.top / value.left) * 180 / Math.PI;
 
-    relPos.forEach(value => {
-        const a = fwd.ortho().slope();
-    });
+            if (value.left < 0)
+                a = 180 + a
 
+            //add the difference in degrees to rots.
+            rots.push(Math.abs(a-rotDeg));
+        });
 
+        //Return the index of the lowest angle.
+        return rots.indexOf(Math.min(...rots));
 
+        //Returns the position of the players in relation to the spinner.
+        function getRelUserPos() {
+            const res = [];
 
+            userpos.forEach(value => res.push({
+                top:  value.top  - s_pos.top,
+                left: value.left - s_pos.left
+            }));
+
+            return res;
+        }
+
+    }
 }
 
 

ab248ec6161e31a7ff33942103e4109dcd62c614
Magnus Harder <magnus@bonusfamilien.dk>
2021-02-17 12:00:26 +0100

    comments added as requested

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,22 @@
+/*
+When website gets loaded socket will be init,
+and sends a packet to the server
+*/
 const socket = io();
+
+
+/*MSG app - delete if making new app*/
 let name = prompt("name");
 const btn = document.getElementById('button');
 const input = document.getElementById('textbox');
 const output = document.getElementById('msg');
 
+
+/*listing to 'connection' port/channel 
+when id gets send from server output will be set to id
+*/
 socket.on('connection', function(id){
+  /*stuff it will do when recieving from 'connection' */
   output.innerHTML = id;
 });
 
@@ -12,11 +24,15 @@ socket.on('connection', function(id){
 
 function send(){
   if(input.value){
+    /*socket.emit sends stuff to the server in this
+     case on 'msg' port it sends input and name */
   socket.emit('msg', input.value, name);
       input.value = '';
   }    
 }
 
+/*receives packet from server
+ when server sends something on 'msg' port this picks it up*/
 socket.on('msg', function(msg, name) {
   let newmsg = document.createElement("p");
   newmsg.innerHTML = name + ": " + msg;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+/*express and node stuff */
 const express = require('express');
 const app = express();
 var path = require('path');
-
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 const PORT = 3000;
 
-/*App Data*/
+/*for doing io stuff*/
+const io = require('socket.io')(http);
+
+
+/*App Data - this should be moved to seperate file*/
 let users = [];
 
 function newID(){
@@ -16,25 +19,26 @@ function newID(){
 }
 
 
-//path som som useren kan få filer fra 
+/*path that clients can use, this means it cant access core server files*/
 app.use('/clientjs', express.static(path.join(__dirname, '/node_modules/socket.io/client-dist')));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/public/js')));
 app.use(express.static(path.join(__dirname, '/public/css')));
 app.use(express.static(path.join(__dirname, '/public/resources')));
 
-
+/*sends index.html to client browser*/
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.use(express.static('/'));
-
+/*io.on is the server listening*/
 io.on('connection', (socket) => {
+  /*when new user connects do this */
   const id = newID();
   console.log('a user connected');
   socket.emit('connection', id);
   socket.on('disconnect', () => {
+    /*when user disconnects do this*/
     console.log('user disconnected')
     for(let i = 0; i < users.length; i++){
       if(users[i].id == id)
@@ -45,18 +49,14 @@ io.on('connection', (socket) => {
 });
 
 io.on('connection', (socket) => {
+  /*receive message from client logs to console and sends back to users*/
   socket.on('msg', (msg, name) => {
     console.log(name + ": " + msg);
-  });
-});
-
-io.on('connection', (socket) => {
-  socket.on('msg', (msg, name) => {
     io.emit('msg', msg, name);
   });
 });
 
-
+/*listens to PORT set on top*/
 http.listen(PORT, () => {
   console.log(`Welcome to Akvario @ *:${PORT}`);
 });

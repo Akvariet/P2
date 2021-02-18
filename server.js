3b8e48c91e78222b8ba3e11e830f7ad56769d389
Magnus Harder <magnus@bonusfamilien.dk>
2021-02-18 16:05:49 +0100

    Users are now sent from server to client and generated on client

--- server.js
+++ server.js
@@ -18,30 +18,43 @@ app.use(express.static(path.join(__dirname, '/public/js')));
 app.use(express.static(path.join(__dirname, '/public/css')));
 app.use(express.static(path.join(__dirname, '/public/resources')));
 
+
+
 /*sends index.html to client browser*/
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 
-/*App code*/
-user.showAll();
 
 /*io.on is the server listening*/
 io.on('connection', (socket) => {
+
+  //sets the socket id to the user id
   socket.id = user.createUser(user.newID());
+
+  //index where this user is in users array
+  const i = user.findIndexID(user.users, socket.id);
+
+  //shows all active ids and free ids
   user.showAll();
   console.log(`user ${socket.id} connected`);
-  user.showNewProp(user.findIndexID(user.users, socket.id));
-  socket.emit('connection', socket.id);
+  user.showNewProp(i);
+
+  //sends the correct user object to client
+  socket.emit('connection', user.users[i]);
 
   socket.on('disconnect', () => {
-    /*when user disconnects do this*/
+    //when user disconnects do this
     console.log(`user ${socket.id} disconnected`);
+
+    //deletes user when client disconnets
     user.deleteID(socket.id);
     user.showAll();
   });
 });
+
+
 
 /*listens to PORT set on top*/
 http.listen(process.env.PORT || PORT, () => {

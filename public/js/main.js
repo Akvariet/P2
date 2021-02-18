15c160558ab598741ef6a10064339a41cf0c83f3
Magnus Harder <magnus@bonusfamilien.dk>
2021-02-18 11:59:21 +0100

    Client Server coms is working

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,14 @@
-let name = "test"; //prompt("Name:");
-const userID = createUser(name);
+const socket = io();
+
+socket.on('connection', function(id){
+  console.log(id);
+});
+
+
+/*App
+const userID = createUser();
 const user = document.getElementById(userID);
 
 move(user);
 
-window.addEventListener("mousemove", function (e) {
-  const user = document.getElementById(userID);
-  const name = document.getElementById(userID + '_name');
-  const space = document.getElementById("space");
-  let mouseX = e.clientX - space.offsetLeft;
-  let mouseY = e.clientY - space.offsetTop;
-  let userX = user.offsetTop - mouseY + 35;
-  let userY = user.offsetLeft - mouseX + 35;
-  let o = users[userID].rad = -1 * Math.atan2(userY, userX);
-  user.style.transform = "rotate(" + o + "rad)";
-  name.style.transform = "rotate(" + -1*o + "rad)";
-}, false);
+window.addEventListener("mousemove", function(e){userRotation(e);}, false);*/

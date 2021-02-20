655456d803ca7c20430344a344d331b0aff3b5ce
Magnus Harder <magnus@bonusfamilien.dk>
2021-02-20 13:25:14 +0100

    fixed comments in server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-/*express and node stuff */
+//express and node stuff
 const express = require('express');
 const app = express();
 const path = require('path');
 const http = require('http').createServer(app);
 const PORT = 3000;
 
-/*for doing io stuff*/
+//for doing io stuff
 const io = require('socket.io')(http);
 
-/*our modules*/
+//our modules
 const user = require("./scripts/user");
 
-/*path that clients can use, this means it cant access core server files*/
+//path that clients can use, this means it cant access core server files
 app.use('/clientjs', express.static(path.join(__dirname, '/node_modules/socket.io/client-dist')));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/public/js')));
@@ -20,14 +20,14 @@ app.use(express.static(path.join(__dirname, '/public/resources')));
 
 
 
-/*sends index.html to client browser*/
+//sends index.html to client browser
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 
 
-/*io.on is the server listening*/
+//io.on is the server listening
 io.on('connection', (socket) => {
 
   //sets the socket id to the user id
@@ -56,7 +56,7 @@ io.on('connection', (socket) => {
 
 
 
-/*listens to PORT set on top*/
+//listens to PORT set on top
 http.listen(process.env.PORT || PORT, () => {
   console.log(`Welcome to Akvario @ *:${PORT}`);
 });

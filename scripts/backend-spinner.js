08843ce78044eb621cc364e6e1a105502f9d4efc
AlexanderSteffensen <asteff20@student.aau.dk>
2021-03-18 10:20:26 +0100

    Fixed calculations error

--- scripts/backend-spinner.js
+++ scripts/backend-spinner.js
@@ -21,15 +21,26 @@ export default function spin(userPos, s_pos){
         ids.forEach(id => {
             let a = Math.atan(relPos[id].top / relPos[id].left) * 180 / Math.PI;
 
+            // if user is in second or third quadrant
             if (relPos[id].left < 0)
-                a = 180 + a
+                a = 180 + a;
 
-            //add the difference in degrees to rots.
-            rots.push(Math.abs(a - (rotDeg % 360)));
+            // if user is in first quadrant
+            if (relPos[id].left > 0 && relPos[id].top < 0)
+                a = 360 + a;
+
+            //calculates the degrees the user has to the rotated spinner
+            let angFromRot = Math.abs(a - (rotDeg % 360))
+
+            if (angFromRot > 180)
+                angFromRot = 360 - angFromRot;
+
+            //adds the difference in degrees to rots.
+            rots.push(angFromRot);
+
         });
 
         //Return the index of the lowest angle.
-
         return ids[rots.indexOf(Math.min(...rots))];
 
         //Returns the position of the players in relation to the spinner.

c08563a83abcfaefc65fe3ae49fbcbbf93c4bf83
Magnus Harder <magnus@bonusfamilien.dk>
2021-04-07 09:31:17 +0200

    Third Party Peer Servers supported

--- public/js/voice.js
+++ public/js/voice.js
@@ -2,6 +2,7 @@ import {options, production} from './clientConfig.js';
 
 //peers contains all connected peers
 export const peers = {}; 
+let myStream;
 
 //handles all peerjs functions and events
 export function handlePeerConnections(myId, users){
@@ -10,15 +11,24 @@ export function handlePeerConnections(myId, users){
     const media = {video: false, audio:true};
 
     //connects to peerjs server
-    const peer  = new Peer(myId, options('voice', production));
+    const peer  = new Peer(myId,options('voice', production));
 
     //gets microphone stream
     getUserMedia(media, streamVoice);
 
     //logs if successfully connected to peer server
-    peer.on('open', myId => console.log("Connected to PeerJS Server with: " + myId));
+    peer.on('open', myId =>{
+        console.log("Connected to PeerJS Server with: " + myId);
+
+        //calls every user already connected to server
+        Object.values(users).forEach(user=>{ 
+            if(myId != user.id)
+            connectToUser(user.id, myStream)
+        });
+    });
 
     function streamVoice(stream){
+        myStream = stream;
         //incoming call event
         peer.on('call', call =>{
     
@@ -30,12 +40,6 @@ export function handlePeerConnections(myId, users){
             call.on('stream', remoteStream=>{
                 startRemoteStream(audio, remoteStream);
             });
-        });
-    
-        //calls every user already connected to server
-        Object.values(users).forEach(user=>{ 
-            if(myId != user.id)
-            connectToUser(user.id, stream)
         });
     }
 

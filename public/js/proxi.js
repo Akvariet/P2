cd5a7b1be2e9f61775c0a7e4297ceea30810800e
Laursen79 <69715502+Laursen79@users.noreply.github.com>
2021-04-07 11:09:16 +0200

    Proximity chat implemented

--- public/js/proxi.js
+++ public/js/proxi.js
@@ -1,7 +1,13 @@
 
+
+function distance(myPosition, position){
+    return dist(relativePos(myPosition, position))
+}
+
+
 // Returns an object of distances between position and the elements in positions.
 // The object is indexed by the user ids.
-export function distance(position, positions){
+export function distances(position, positions){
     const temp = {};
 
     Object.keys(positions).forEach(id => {
@@ -10,40 +16,60 @@ export function distance(position, positions){
 
     return temp;
 
-    // Returns distance given a relative position.
-    function dist(relPos){
-        return Math.sqrt((relPos.left * relPos.left) + (relPos.top * relPos.top));
-    }
+}
 
-    // Returns the position of pos2 relative to pos1
-    function relativePos(pos1, pos2){
-        const pos = {};
-        pos['top']  = pos2.top  - pos1.top;
-        pos['left'] = pos2.left - pos1.left;
-        return pos;
-    }
+// Returns distance given a relative position.
+function dist(relPos){
+    return Math.sqrt((relPos.left * relPos.left) + (relPos.top * relPos.top));
+}
+
+// Returns the position of pos2 relative to pos1
+function relativePos(pos1, pos2){
+    const pos = {};
+    pos['top']  = pos2.top  - pos1.top;
+    pos['left'] = pos2.left - pos1.left;
+    return pos;
 }
 
 // Contains all of the functions that control how voice volume change according to distance.
 export class VolumeFunctions {
     constructor(slope, minVolume) {
-        this.slope = slope || 0.075;
-        this.minVolume = minVolume || 20;
-        this.maxVolume = 100;
+        this.slope = slope || 0.0005;
+        this.minVolume = minVolume || 0.05;
+        this.maxVolume = 1;
     }
     //Volume functions
-    volume = volume => volume > this.minVolume ? volume : this.minVolume;
+    volumeCap(volume){
+        if (volume < this.minVolume)
+            return this.minVolume;
+        else if (volume > this.maxVolume)
+            return this.maxVolume
+        return volume;
+    }
 
-    linearDecrease = distance => this.volume(-this.slope * distance + this.maxVolume);
+    linearDecrease(distance){
+        const volume = -this.slope * distance + this.maxVolume;
+        return this.volumeCap(volume);
+    }
 }
 
 // Recalculate the volume of all users.
 // Useful when this client moves and the relative positions of everyone has changed.
 export function calculateVolume(myPosition, userCollection, volumeFunc){
-    const dist = distance(myPosition, userCollection.positions());
+    const dist = distances(myPosition, userCollection.positions());
 
     Object.keys(dist).forEach(key => (userCollection.get(key)).volume = volumeFunc(dist[key]));
 }
 
+const volFunc = new VolumeFunctions();
 
+export function adjustVolume(audio, myPosition, position){
+    const d = distance(myPosition, position);
+    const vol = volFunc.linearDecrease(d);
+    audio.volume = vol;
+}
 
+export function getPos(HTMLElement){
+    return {top:  Number(HTMLElement.style.top.slice(0, -2)),
+        left: Number(HTMLElement.style.left.slice(0, -2)) };
+}
--- public/js/voice.js
+++ public/js/voice.js
@@ -1,70 +1,114 @@
 import {options, production} from './clientConfig.js';
+import {adjustVolume, getPos} from './proxi.js';
+import {connection} from './main.js';
 
 //peers contains all connected peers
-export const peers = {}; 
+export const peers = {};
+const audioPlayers = {};
+let myId;
 
 //handles all peerjs functions and events
-export function handlePeerConnections(myId, users){
-    
+export function handlePeerConnections(id, users) {
+
+    myId = id;
+
     const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    const media = {video: false, audio:true};
+    const media = {video: false, audio: true};
 
     //connects to peerjs server
-    const peer  = new Peer(myId, options('voice', production));
+    const peer = new Peer(myId, options('voice', production));
 
     //gets microphone stream
     getUserMedia(media, streamVoice);
 
     //logs if successfully connected to peer server
-    peer.on('open', myId => console.log("Connected to PeerJS Server with: " + myId));
+    peer.on('open', myId => {
+        console.log("Connected to PeerJS Server with: " + myId);
+        beginProxiChat(myId);
+    });
 
-    function streamVoice(stream){
+    function streamVoice(stream) {
         //incoming call event
-        peer.on('call', call =>{
-    
+        peer.on('call', call => {
+
             //must be answered
             call.answer(stream);
-    
+
             //when incoming call adds a stream
             const audio = document.createElement('audio');
-            call.on('stream', remoteStream=>{
+
+            proxiChat(audio, call.peer);
+
+            call.on('stream', remoteStream => {
                 startRemoteStream(audio, remoteStream);
             });
         });
-    
+
         //calls every user already connected to server
-        Object.values(users).forEach(user=>{ 
-            if(myId != user.id)
-            connectToUser(user.id, stream)
+        Object.values(users).forEach(user => {
+            if (myId != user.id)
+                connectToUser(user.id, stream)
         });
     }
 
     //plays remote stream through audio object
-    function startRemoteStream(audio, remoteStream){
+    function startRemoteStream(audio, remoteStream) {
         audio.srcObject = remoteStream;
         audio.addEventListener('loadedmetadata', playRemoteStream);
-    
-        function playRemoteStream(){ audio.play(); }
+
+        function playRemoteStream() {
+            audio.play();
+        }
     }
-    
-    function connectToUser(newUserID, stream){
+
+    function connectToUser(newUserID, stream) {
 
         //makes call to remote peer
         const call = peer.call(newUserID, stream);
 
-        //when remote peer as answered and added a stream
+        //when remote peer has answered and added a stream
         const audio = document.createElement('audio');
-        call.on('stream', remoteStream=>{
+        proxiChat(audio, newUserID);
+
+        call.on('stream', remoteStream => {
             startRemoteStream(audio, remoteStream);
         });
-    
+
         call.on('close', removeRemoteStream);
-    
+
         //stores call in object
         peers[newUserID] = call;
 
-        function removeRemoteStream(){
+        function removeRemoteStream()
+        {
             audio.remove();
         }
     }
 }
+function beginProxiChat(myID){
+    const myElement = document.getElementById(myID);
+    myElement.addEventListener('moved', (e)=>{
+
+        for (const audioPlayer in audioPlayers) {
+            adjustVolume(audioPlayers[audioPlayer].audio, e.detail, audioPlayers[audioPlayer].position);
+        }
+    });
+}
+
+function proxiChat(audio, userID){
+
+    const userContainer = document.getElementById(userID);
+    const myElement = document.getElementById(connection.myID);
+    userContainer.append(audio);
+    audioPlayers[userID] = {audio: audio, position: getPos(userContainer)};
+
+    userContainer.addEventListener('moved', (e) => {
+        const pos1 = getPos(myElement);
+        const pos2 = getPos(userContainer);
+
+        audioPlayers[userID].position = pos2;
+
+        adjustVolume(audio, pos1, pos2);
+    })
+}
+

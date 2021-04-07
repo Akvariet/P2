7e1a077897fed2a16eb20812c807067f3ad1e133
Magnus Harder <magnus@bonusfamilien.dk>
2021-04-07 09:39:20 +0200

    Revert "Third Party Peer Servers supported"
    This reverts commit c08563a83abcfaefc65fe3ae49fbcbbf93c4bf83.

--- scripts/serverConfig.js
+++ scripts/serverConfig.js
@@ -1,12 +1,6 @@
-/**************************
-* Configuration of Server *
-* Our AAU ports 3200-3209 *
-* Change it to 3000 if it *
-* is confusing            *
-**************************/
-
 //server.js
 export const peerJSOptions = { port:3201 }
 
+// port range: 3200-3209
 export const port = process.env.PORT || 3200;
 

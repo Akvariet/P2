2bcbc40d04086ebda04cc2dd046d22dcfaf4535a
AlexanderSteffensen <asteff20@student.aau.dk>
2021-04-13 14:47:05 +0200

    added comments and changed the data type from sound waves to frequency bars

--- public/js/voiceAnalysis.js
+++ public/js/voiceAnalysis.js
@@ -1,9 +1,7 @@
 import {connection} from "./ClientConnection.js";
 
-const minBuffer = 140;
-
 export function analyzeVoice(stream) {
-    let drawVisual;
+    let newAnalysis;
     let speaking = false;
     const userElement = document.getElementById(connection.myID + '_body');
 
@@ -16,7 +14,7 @@ export function analyzeVoice(stream) {
     source.connect(analyser);
 
     //Sets the frequency of the fft (Fast Fourier Transform)
-    analyser.fftSize = 2048;
+    analyser.fftSize = 256;
 
     // Finds the amount of data points that will be collected
     let bufferLength = analyser.frequencyBinCount;
@@ -27,24 +25,31 @@ export function analyzeVoice(stream) {
     // A function that returns the larges element in array
     let max = (array) => Math.max(...array);
 
+    // sets the range of sounds that we want to get. sounds < -40 is 0 in the data array, and sounds > -10 is 255 (max value)
+    analyser.maxDecibels = -10;
+    analyser.minDecibels = -40;
+
     function analyse() {
-        drawVisual = requestAnimationFrame(analyse);
+        //run this function before next repaint
+        newAnalysis = requestAnimationFrame(analyse);
 
-        // Gets the waveform data of the analyser and puts it into the dataArray
-        analyser.getByteTimeDomainData(dataArray);
+        // Gets the sound data of the analyser and puts it into the dataArray
+        analyser.getByteFrequencyData(dataArray);
 
+        //find the highest value in the data array
         let max_data = max(dataArray);
 
-        if (max_data > minBuffer){
+        //If there is a sound in the range
+        if (max_data){
             if (speaking === false){
                 speaking = true;
-                userElement.classList.add('speaking');
+                userElement.classList.add('speaking'); // So the user can see that he/she is speaking
                 connection.emit("user-speaking", speaking, connection.myID);
             }
         }
         else if (speaking === true) {
             speaking = false;
-            userElement.classList.remove('speaking');
+            userElement.classList.remove('speaking'); // So the user can see that he/she stopped speaking
             connection.emit("user-speaking", speaking, connection.myID);
         }
     }
@@ -52,9 +57,9 @@ export function analyzeVoice(stream) {
 }
 
 export function displayUserSpeak(speaking, id) {
-    console.log(id);
     const userElement = document.getElementById(id + '_body');
 
+    //If the user started speaking, add the class, if the user stopped speaking, remove the class.
     if (speaking) userElement.classList.add('speaking');
     else userElement.classList.remove('speaking');
 }

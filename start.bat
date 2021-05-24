bebfefd670ef34562247c34dcd2cee5cc45896c4
MJHC <magnus@bonusfamilien.dk>
2021-05-24 13:34:41 +0200

    bat script added

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+title Akvario Test
+start cmd.exe /k node ./test/testServer.js
+start cmd.exe /k node server.js -T
+timeout 1
+start cmd.exe /k node ./test/client/testClient.js
+start cmd.exe /k node ./test/client/testClient.js
+pause
+exit
